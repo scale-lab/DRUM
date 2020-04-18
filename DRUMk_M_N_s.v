210a489fb4f14822f419d97cf7c9b24a09914b76
Sherief Reda <sherief_reda@brown.edu>
2020-04-17 21:46:01 -0400

    Update DRUMk_M_N_s.v

--- DRUMk_M_N_s.v
+++ DRUMk_M_N_s.v
@@ -1,6 +1,7 @@
 `timescale 1ns / 1ps
 /*
 Copyright (c) 2015 Soheil Hashemi (soheil_hashemi@brown.edu)
+              2018 German Research Center for Artificial Intelligence (DFKI)
 
 Permission is hereby granted, free of charge, to any person
 obtaining a copy of this software and associated documentation
